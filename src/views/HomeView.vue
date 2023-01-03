efe1475b0a12262ec69aa6e2a65ac8ff76ba5029
viniciusgbsantos <vinicius.gabriel001@outlook.com>
2023-01-02 22:19:19 -0300

    Update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,30 @@
 <template>
-  <div>
-  </div>
+  <section>
+    <h1>👍 Compre ou Venda 👎</h1>
+    <ProdutosBuscar />
+    <ProdutosLista />
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
+import ProdutosBuscar from "@/components/ProdutosBuscar.vue";
+import ProdutosLista from "@/components/ProdutosLista.vue";
 
 export default {
-  name: 'HomeView',
-
-}
+  name: "HomeView",
+  components: {
+    ProdutosBuscar,
+    ProdutosLista
+  }
+};
 </script>
+
+<style scoped>
+h1 {
+  color: #87f;
+  margin-top: 40px;
+  font-size: 2rem;
+  text-align: center;
+}
+</style>
+
